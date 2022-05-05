f65d8827d2a438838a54dde923a328d7fd8228db
Charlie <kaizewu@uchicago.edu>
2022-05-05 14:59:55 -0500

    add imports in config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('/Users/apple/Desktop/tsdistance/tsdistance'))
+autodoc_mock_imports = ["tsdistance"]
 
 
 # -- Project information -----------------------------------------------------
